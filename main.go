fecce6c519f549e47e92a84ac439cd32a82a5a15
Kosuke Takahashi <takohashi0410@gmail.com>
2024-03-20 18:15:40 +0900

    :sparkles: variables

--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
-// format package
-// printとかを提供する標準のパッケージ
 import "fmt"
 
 // Hello world
 
 func main() {
-	fmt.Println("Hello world")
+	// fmt.Println("Hello world")
+	// fmt.Println(time.Now())
+
+	// var 変数名　型　=　値
+
+	var i int = 100
+	fmt.Println(i)
+
+	var s string = "Hello world"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t)
+	fmt.Println(f)
+
+	var (
+		i2 int    = 200
+		s2 string = "test"
+	)
+
+	fmt.Println(i2)
+	fmt.Println(s2)
+
+	// 暗黙的な定義
+	// 変数名 := 値
+	testvalue := "string type"
+	fmt.Println(testvalue)
 }
